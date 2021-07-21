c8921e428eec6207d318d2b24bedcafb5af15c82
harrisonychung <70103285+harrisonychung@users.noreply.github.com>
2021-07-21 17:31:40 -0400

    Updated discord.py

--- discord.py
+++ discord.py
@@ -1,2 +1,21 @@
 #starting my discord bot which compliments a user. In the future I'd like it to remind the user to drink water every x minutes.
 
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$Hello'):
+    await message.channel.send('Hello!')
+
+
+client.run(os.environ['TOKEN'])
