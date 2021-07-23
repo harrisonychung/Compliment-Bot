254b750bab4f0ece8e5195f35cfd15078ecd4eea
harrisonychung <70103285+harrisonychung@users.noreply.github.com>
2021-07-23 12:43:52 -0400

    Update discord.py

--- discord.py
+++ discord.py
@@ -4,8 +4,17 @@ import discord
 import os
 import requests
 import json
+import random
 
 client = discord.Client()
+
+sad_words = ["depressed", "unhappy", "sad", "melancholy", "miserable", "downcast", "dejected"]
+
+starter_encouragements = [
+  "Cheer up!",
+  "Hang in there!",
+  "You are a great person!"
+]
 
 def get_quote(): 
   response = requests.get("https://zenquotes.io/api/random")
@@ -21,10 +30,15 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
+  
+  msg = message.content
 
-  if message.content.startswith('$inspire'):
+  if msg.startswith('$inspire'):
     quote = get_quote()
     await message.channel.send(quote)
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(starter_encouragements))
 
 
 client.run(os.environ['TOKEN'])
